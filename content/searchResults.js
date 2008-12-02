53d13d41ca76f71f306c25b2950cb14e697a56ce
Bryan Clark <clarkbw@gnome.org>
2008-12-02 15:22:43 -0800

    initial change over to the new list layout need to fix the checkbox selection

--- content/searchResults.js
+++ content/searchResults.js
@@ -4,36 +4,26 @@ let Ci = Components.interfaces;
 
 function OnLoad() {
   try {
-    $("#toolbox").width($(".toolbox").width());    
-    positionToolbox();
-    $(".conversation:first-child").focus();    
-    $(window).resize(function() { positionToolbox(); });
+    $(".conversations").css("marginTop", $("#toolbox").height() );
+    $(".toolbox").width($(".conversations").width());
+    
+    $(window).resize(function() { $("#toolbox").width($(".conversations").width()); });
+    
     $("button.archive").click(function() {
-      removeConversation(selectedConversation);
+      removeConversations();
     });
     $("button.delete").click(function() {
-      removeConversation(selectedConversation);
+      removeConversations();
     });
     $("button.junk").click(function() {
-      removeConversation(selectedConversation);
+      removeConversations();
     });
-    $("button.reply").click(function() {});
-    $("button.forward").click(function() {});
   } catch (e) {
     dumpExc(e);
   }
 }
 
-function removeConversation(obj) {
-  obj.slideUp("fast");
-  selectedConversation = $(obj[0].nextElementSibling);
+function removeConversations() {
+  $(":checked").parent().slideUp("fast");
 }
-
-function positionToolbox() {
-  dump("in positionToolbox!\n");
-  $("#toolbox").css("left", $(".toolbox").offset().left);
-}
-
-var selectedConversation = null;
-
 
